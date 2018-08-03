929452917ca3f8971a8614644d7a90acb3679b54
cirun <cirunzhang@gmail.com>
2018-08-03 14:41:47 +0800

    add more GUI components

--- src/GUI/Chessboard.java
+++ src/GUI/Chessboard.java
@@ -2,23 +2,51 @@ package GUI;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-
 public class Chessboard extends JPanel {
-    private static BufferedImage board;
+    private  BufferedImage board;
+    private  BufferedImage black;
+    private  BufferedImage white;
+    //
+    private int x = 0;
+    private int y = 0;
 
-    public static void init() throws Exception{
-        board = ImageIO.read(new File("/Users/cirun/Documents/admin/code/java/project/src/GUI/assets/chessboard.jpg"));
+    public void init() throws Exception{
+        this.board = ImageIO.read(new File("/Users/cirun/Documents/admin/code/java/project/src/GUI/assets/chessboard.jpg"));
+        this.black = ImageIO.read(new File("/Users/cirun/Documents/admin/code/java/project/src/GUI/assets/black.png"));
+        this.white = ImageIO.read(new File("/Users/cirun/Documents/admin/code/java/project/src/GUI/assets/white.png"));
+        enter();
+
+    }
+
+    public void enter(){
+        this.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+
+                System.out.println(e.getX());
+                System.out.println(e.getY());
+                x = e.getX();
+                y = e.getY();
+                repaint();
+            }
+        });
     }
 
     @Override
-    public void paint(Graphics g ){
-        try {
-            init();
-        }catch (Exception e){
-            System.out.println("Och!!");
-        }
+    public void paint(Graphics g){
         g.drawImage(board, 0, 0, null);
+        g.drawImage(black,x,y,null);
+        /*
+        g.drawImage(black,7,11,null);
+        g.drawImage(white,7,45,null);
+        g.drawImage(black,42,11,null);
+        g.drawImage(white,42,45,null);
+        g.drawImage(black,77,11,null);
+        */
     }
+
 }
--- src/GUI/Game.java
+++ src/GUI/Game.java
@@ -4,15 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Game {
+    // width and height of the chessboard image
     private final int BOARD_WIDTH = 535;
     private final int BOARD_HETGHT = 536;
-    JFrame frame = new JFrame("五子棋游戏");
-    public void start(){
+    private JFrame frame = new JFrame("Gomoku");
+    public void start() throws Exception{
         Chessboard chessboard = new Chessboard();
+        chessboard.init();
 
         chessboard.setPreferredSize(new Dimension(BOARD_WIDTH , BOARD_HETGHT));
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocation(415, 95);
         frame.add(chessboard);
         frame.pack();
